c84c4405dedd517ed8553ad7d5226376d471e941
Tomasz Kalisiak <tkalisiak@gmail.com>
2022-10-04 14:20:58 +0200

    init: initial commit CR-1

---
+++ src/test/java/com/ngworks/loanengine/LoanEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngworks.loanengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
